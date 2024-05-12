5157fe5fd6c0ac99a132c0ffa2c1745edef14251
AmanJha1105 <amanjha1105@gmail.com>
2024-05-13 01:10:09 +0530

    setting up sign up page functionality

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -7,9 +7,9 @@ import { Link } from 'react-router-dom'
 import Logout from './Logout';
 
 const Sidebar = () => {
-  const authUser=true;
+  const authUser=false;
   return (
-    <aside className='flex flex-col items-center min-w-12 sm:w-16 sticky top-0 left-0 h-screen py-8 overflow-y-auto border-r bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 hover:bg-gray-600/10 border-gray-300 text-white'>
+    <aside className='flex flex-col items-center min-w-12 sm:w-16 sticky top-0 left-0 h-screen py-8 overflow-y-auto border-r bg-glass'>
         <nav className='h-full flex flex-col gap-3'>
             <Link to='/'className='flex justify-center'>
                 <img className='h-8' src='/github.svg' alt='Github Logo'/>
